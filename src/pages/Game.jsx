01f5e72cb4f70ecff2defa2cc95e35ca20dbff01
kyaki <emanuelrs101@gmail.com>
2024-03-30 20:55:24 -0600

    Fixed glitch with 9 dobble

--- src/pages/Game.jsx
+++ src/pages/Game.jsx
@@ -34,7 +34,7 @@ function Game(){
                 array[arr[i].id_card].info.push(temp)
             }
 
-            let fours = array.slice(390 - 71, 390)
+            let fours = array.slice(318, 390)
 
             fours = fours.sort(() => Math.random() - 0.5)
 
